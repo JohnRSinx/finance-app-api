afda113a1601a3b920b2943d492fa4f882cf3ae0
John Rodrigues <johnrodrigues97106@gmail.com>
2024-05-23 16:51:15 -0300

    chore:add pg and dotenv setup

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+import express from 'express';
+import 'dotenv/config.js'
+import { PostgresHelper } from './src/db/postgres/helper.js';
+
+const app = express();
+
+app.get('/', async (req, res) => {
+  try {
+    const results = await PostgresHelper.query('SELECT * FROM users;');
+    res.send(results);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
+});
 
 
+app.listen(3000, () =>
+  console.log(`listening on port 3000`)
+);
